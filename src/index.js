ae6ad75965b9b8a663d5e20d0837b841234eb934
Vincent-Mpenyana <vincent.mpenyana8@gmail.com>
2020-10-01 18:56:36 +0200

    updating serviceWorker for Offline capability

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { createLogger } from 'redux-logger';
 import 'tachyons';
 
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import { requestRobots, searchRobots } from './reducers'
 
 import './index.css';
@@ -24,4 +24,9 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
