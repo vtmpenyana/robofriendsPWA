c2ca88fa8c46f85d8af7d64b64775f2ce7fe6fe4
Vincent-Mpenyana <vincent.mpenyana8@gmail.com>
2020-10-01 18:10:34 +0200

    adding aria-label to input

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,9 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
+
       <input
+        aria-label="Search Field"
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='search robots'
